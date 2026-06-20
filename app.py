8efdfc09d42bc327f23d1300cb85d68b991cf210
Kirubanithi10 <kirubanithi1095@gmail.com>
2026-06-20 13:15:22 +0000

    fix: remove unused flask request import

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 SHIPMENTS = {
     "CG001": {"origin": "Chennai", "destination": "Dubai",
